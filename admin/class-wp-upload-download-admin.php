74f6b101d8b96e2e1be70f7d42f00573ef4bd0a0
sue <sue@srcginc.com>
2018-01-08 08:57:50 -0800

    Include plugin on admin page list and create settings link.

--- admin/class-wp-upload-download-admin.php
+++ admin/class-wp-upload-download-admin.php
@@ -100,4 +100,58 @@ class Wp_Upload_Download_Admin {
 
 	}
 
+
+/**
+These three plugins came from https://scotch.io/tutorials/how-to-build-a-wordpress-plugin-part-1#toc-plugin-boilerplate-folder-structure
+**/
+
+/**
+ * Register the administration menu for this plugin into the WordPress Dashboard menu.
+ *
+ * @since    1.0.0
+ */
+
+public function add_plugin_admin_menu() {
+
+    /*
+     * Add a settings page for this plugin to the Settings menu.
+     *
+     * NOTE:  Alternative menu locations are available via WordPress administration menu functions.
+     *
+     *        Administration Menus: http://codex.wordpress.org/Administration_Menus
+     *
+     */
+    add_options_page( 'WP Cleanup and Base Options Functions Setup', 'WP Cleanup', 'manage_options', $this->plugin_name, array($this, 'display_plugin_setup_page')
+    );
 }
+
+ /**
+ * Add settings action link to the plugins page.
+ *
+ * @since    1.0.0
+ */
+
+public function add_action_links( $links ) {
+    /*
+    *  Documentation : https://codex.wordpress.org/Plugin_API/Filter_Reference/plugin_action_links_(plugin_file_name)
+    */
+   $settings_link = array(
+    '<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
+   );
+   return array_merge(  $settings_link, $links );
+
+}
+
+/**
+ * Render the settings page for this plugin.
+ *
+ * @since    1.0.0
+ */
+
+public function display_plugin_setup_page() {
+    include_once( 'partials/wp-upload-download-admin-display.php' );
+}
+
+
+}
+
