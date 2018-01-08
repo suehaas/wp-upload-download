a408a8f643c322b9cf59bd0c135df70e45e49ae7
sue <sue@srcginc.com>
2018-01-08 11:24:27 -0800

    Reverting to files as they were in commit 7df1aa6212f43f134322774878646df730ddbfa6

--- admin/class-wp-upload-download-admin.php
+++ admin/class-wp-upload-download-admin.php
@@ -178,36 +178,8 @@ public function validate($input) {
     $valid['jquery_cdn'] = (isset($input['jquery_cdn']) && !empty($input['jquery_cdn'])) ? 1 : 0;
     $valid['cdn_provider'] = esc_url($input['cdn_provider']);
 
-        // Login Customization
-        //First Color Picker
-        $valid['login_background_color'] = (isset($input['login_background_color']) && !empty($input['login_background_color'])) ? sanitize_text_field($input['login_background_color']) : '';
-
-        if ( !empty($valid['login_background_color']) && !preg_match( '/^#[a-f0-9]{6}$/i', $valid['login_background_color']  ) ) { // if user insert a HEX color with #
-            add_settings_error(
-                    'login_background_color',                     // Setting title
-                    'login_background_color_texterror',            // Error ID
-                    'Please enter a valid hex value color',     // Error message
-                    'error'                         // Type of message
-            );
-        }
-
-        //Second Color Picker
-        $valid['login_button_primary_color'] = (isset($input['login_button_primary_color']) && !empty($input['login_button_primary_color'])) ? sanitize_text_field($input['login_button_primary_color']) : '';
-
-        if ( !empty($valid['login_button_primary_color']) && !preg_match( '/^#[a-f0-9]{6}$/i', $valid['login_button_primary_color']  ) ) { // if user insert a HEX color with #
-            add_settings_error(
-                    'login_button_primary_color',                     // Setting title
-                    'login_button_primary_color_texterror',            // Error ID
-                    'Please enter a valid hex value color',     // Error message
-                    'error'                         // Type of message
-            );
-        }
-
-        //Logo image id
-        $valid['login_logo_id'] = (isset($input['login_logo_id']) && !empty($input['login_logo_id'])) ? absint($input['login_logo_id']) : 0;
-
-        return $valid;
-    }
+    return $valid;
+ }
 
 
 
